f996f88014f3ef8d0da3a8cac03936b72b2232f5
Wei-Hao Wang <wwang03@risd.edu>
2020-05-09 22:24:53 -0400

    map

--- js/anchor_offset.js
+++ js/anchor_offset.js
@@ -51,6 +51,8 @@ function anchor_offset(post_type){
 					newAnchor_position[i] = entry_content_top + el.offsetTop;
 				});
 				var footer = document.querySelector(".footer-meta.post-meta");
+				if(typeof footer == 'undefined')
+					footer = document.getElementById("colophon");
 				newAnchor_position.push(entry_content_top + footer.offsetTop);
 				return newAnchor_position;
 			}
