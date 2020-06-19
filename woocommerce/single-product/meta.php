9e460d256f832913770c98fa052613c229f9694e
Wei-Hao Wang <wwang03@risd.edu>
2020-06-19 17:57:08 -0400

    remove tag in product pages

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -45,7 +45,7 @@ global $product;
 		}
 	?>
 
-	<?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+	<!-- <?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?> -->
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
